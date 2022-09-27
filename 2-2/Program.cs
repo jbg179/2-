ff72cb114322fadf7130cb0865c628c80d42bb52
jbg179 <jbg179@naver.com>
2022-09-27 23:25:01 +0900

    2-2

---
+++ 2-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class IntegerContantApp
+{
+    public static void Main()
+    {
+        int i= 255,h = 0xff;
+        long l = 0xffL;
+        Console.WriteLine("i={0}, h= {1}", i, h);
+        if (h == l) Console.WriteLine("Yes");
+        else Console.WriteLine("No");
+    }
+}
