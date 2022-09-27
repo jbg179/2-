62133c54af405c23fd208088e0a4b8793ccde63f
jbg179 <jbg179@naver.com>
2022-09-27 23:53:23 +0900

    2-10

---
+++ 2-10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class ArrayTypeApp
+{
+    public static void Main()
+    {
+        int[] ia = new int[3];
+        int[] ib = { 1, 2, 3 };
+        int i;
+        for (i = 0; i < ia.Length; i++)
+            ia[i] = ib[i];
+        for (i = 0; i < ia.Length; i++)
+            Console.Write(ia[i] + " ");
+        Console.WriteLine();
+    }
+}
