9b4141a275975dcc5f3f122037fde41eccdf1bda
jbg179 <jbg179@naver.com>
2022-09-28 23:55:38 +0900

    2-16

---
+++ 2-16/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class LogicalOperatorApp
+{
+    public static void Main()
+    {
+        Console.WriteLine("TRUE AND TRUE = " + (true && true));
+        Console.WriteLine("TRUE AND FALSE = " + (true && false));
+        Console.WriteLine("FALSE AND TRUE = " + (false && true));
+        Console.WriteLine("FALSE AND FALSE = " + (false && false));
+    }
+}
