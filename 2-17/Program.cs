59938ac3e24e9cd30de7508ff4f6ccaef75c98a3
jbg179 <jbg179@naver.com>
2022-09-29 00:06:35 +0900

    2-17

---
+++ 2-17/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class LogicalOperatorApp
+{
+    public static void Main()
+    {
+        int x = 3, y = 5, z = 7;
+        bool b;
+        b = x < y && y < z;
+        Console.WriteLine("Result = " + b);
+        b = x == y || x < y && y > z;
+        Console.WriteLine("Result = " + b);
+    }
+}
