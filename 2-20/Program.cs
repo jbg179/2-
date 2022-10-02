ab1bdfe1fa71527fa871021a163c2c60a3879a4f
jbg179 <jbg179@naver.com>
2022-10-02 19:06:03 +0900

    2-20

---
+++ 2-20/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class ShiftOperatorApp
+{
+    public static void Main()
+    {
+        Console.WriteLine(" 10 << 2 = " + (10 << 2));
+        Console.WriteLine(" 10 >> 2 = " + (10 >> 2));
+    }
+}
