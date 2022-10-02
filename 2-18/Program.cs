04916b199dd28aec8199f8a5bc2c63d2e7983b0e
jbg179 <jbg179@naver.com>
2022-10-02 18:36:58 +0900

    2-18

---
+++ 2-18/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class IncDecOperatorsApp
+{
+    public static void Main()
+    {
+        int x = 3, y = 5;
+        int a, b;
+        ++x; --y;
+        Console.WriteLine("x = " + x + " , y = " + y);
+        a = (++x) + 1;
+        Console.WriteLine("x = " + x + " , a = " + a);
+        b = (y++) + 1;
+        Console.WriteLine("y = " + y + " , b = " + b);
+    }
+}
