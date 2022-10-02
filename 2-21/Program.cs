c637bed8e29eccc8fd53f39986c826475103b256
jbg179 <jbg179@naver.com>
2022-10-02 19:10:39 +0900

    2-21

---
+++ 2-21/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class ConditionalOperatorApp
+{
+    public static void Main()
+    {
+        int a, b, c;
+        int m;
+        Console.Write("Enter three numbers : ");
+        a = Console.Read() - '0';
+        b = Console.Read() - '0';
+        c = Console.Read() - '0';
+        m = (a > b) ? a : b;
+        m = (m > c) ? m : c;
+        Console.WriteLine("The largest number = " + m);
+    }
+}
